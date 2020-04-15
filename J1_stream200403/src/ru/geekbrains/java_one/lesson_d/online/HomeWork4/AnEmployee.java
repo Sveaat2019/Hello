5f11238a09833ac507a23522e83cc7affdd04e16
Sveatoslav Makushkin <sveaat2019@gmail.com>
2020-04-15 14:53:30 +0200

    Начало домашки №4.

---
+++ J1_stream200403/src/ru/geekbrains/java_one/lesson_d/online/HomeWork4/AnEmployee.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.java_one.lesson_d.online.HomeWork4;
+
+public class AnEmployee {
+    private String name;
+    private double salary;
+    private int age;
+
+}
---
+++ J1_stream200403/src/ru/geekbrains/java_one/lesson_d/online/HomeWork4/HomeWork4.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.java_one.lesson_d.online.HomeWork4;
+
+public class HomeWork4 {
+
+    public static void main(String[] args) {
+
+        AnEmployee man = new AnEmployee();
+    }
+}
